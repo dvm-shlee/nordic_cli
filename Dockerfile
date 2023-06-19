61c202cfd672aee58902c8f92320925132942020
dvm-shlee <shlee@unc.edu>
2023-06-18 21:56:07 -0400

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,4 @@
-FROM ubuntu:latest
-
-# Update package lists
-RUN apt update
-
-# Install required packages
-RUN apt install -q -y --no-install-recommends xorg python3 python3-pip git unzip wget curl
-
-# Clean up package lists
-RUN apt-get clean && rm -rf /var/lib/apt/lists/*
-
-# Download the MCR from MathWorks site
-RUN mkdir /mcr-src && \
-    cd /mcr-src && \
-    wget --no-check-certificate -q https://ssd.mathworks.com/supportfiles/downloads/R2023a/Release/2/deployment_files/installer/complete/glnxa64/MATLAB_Runtime_R2023a_Update_2_glnxa64.zip
-
-# Extract the downloaded MCR
-RUN cd /mcr-src && \
-    unzip -q MATLAB_Runtime_R2023a_Update_2_glnxa64.zip && \
-    rm -f MATLAB_Runtime_R2023a_Update_2_glnxa64.zip
-
-# Install the MCR with silent mode and agree to the license
-RUN cd /mcr-src && \
-    ./install -agreeToLicense yes -mode silent -destinationFolder /usr/local/mcr
-
-# Clean up the installation files
-RUN rm -rf /mcr-src
-
-# Configure environment variables for MCR
-ENV LD_LIBRARY_PATH /usr/local/mcr/v914/runtime/glnxa64:/usr/local/mcr/v914/bin/glnxa64:/usr/local/mcr/v914/sys/os/glnxa64:/usr/local/mcr/v914/extern/bin/glnxa64
-ENV PATH /usr/local/bin
+FROM mcr:2023a
 
 RUN git clone https://github.com/dvm-shlee/nordic_cli.git /app
 WORKDIR /app
@@ -36,7 +6,7 @@ RUN pip3 install poetry
 RUN pip3 install -e .
 
 # For Command Line Tool mode with input arguments provided
-ENTRYPOINT [ "executable" ]
+ENTRYPOINT [ "nordic" ]
 
 # For Interactive mode
 CMD ["bash"]
--- nordic/scripts/nordic.py
+++ nordic/scripts/nordic.py
@@ -1,13 +1,7 @@
 import argparse
 from .. import run
 
-def main(input_file, output_file, uppercase):
-    # Your code logic here
-    print(f"Input file: {input_file}")
-    print(f"Output file: {output_file}")
-    print(f"Uppercase: {uppercase}")
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description='Command line tool(CLI) for NORDIC Denoising')
     
     parser.add_argument('magni_path', help='Path to the input magnitude image file')
@@ -15,8 +9,8 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--phase_path', help='Path to the input phase image file')
     parser.add_argument('-m', '--modality', help='Indicates the image modality - "fMRI" or "dMRI"')
     parser.add_argument('-t', '--threshold_method', default='NORDIC', help='Determines the thresholding method - "NORDIC" or "MP"')
-    parser.add_argument('--kernel_size_gfactor', nargs=3, type=int, default=[14, 14, 1], help='Specifies the kernel size for the g-factor')
-    parser.add_argument('--kernel_size_pca', nargs=3, type=int, help='Specifies the kernel size for PCA')
+    parser.add_argument('--kernel_size_gfactor', nargs=3, type=int, default=[14, 14, 1], metavar=('sizex', 'sizey', 'sizez'), help='Specifies the kernel size for the g-factor')
+    parser.add_argument('--kernel_size_pca', nargs=3, type=int, metavar=('sizex', 'sizey', 'sizez'), help='Specifies the kernel size for PCA')
     
     args, unknown_args = parser.parse_known_args()
     
@@ -32,3 +26,6 @@ if __name__ == '__main__':
     kwargs = dict(arg.split('=') for arg in unknown_args)
     
     run(magni_path, output_path, phase_path, modality, threshold_method, kernel_size_gfactor, kernel_size_pca, **kwargs)
+    
+if __name__ == "__main__":
+    main()
